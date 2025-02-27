d98484144f5dd645ba3d48cff914c3afdd6dbad7
Md. Saminul Amin <saminul.amin@gmail.com>
2025-02-28 00:24:25 +0600

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
 // import "./App.css";
+import Dashboard from "./pages/dashboard/Dashboard";
 import Form from "./pages/form/Form";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("user:token") !== null || true;
+
+  if (!isLoggedIn) {
+    return <Navigate to={"/users/sign_in"} />;
+  } else if (
+    isLoggedIn &&
+    ["/users/sign_in", "/users/sign_up"].includes(window.location.pathname)
+  ) {
+    return <Navigate to={"/"} />;
+  }
+
+  return children;
+};
 
 function App() {
   return (
-    <div className="bg-[#e1edff] h-screen flex justify-center items-center">
-      <Form />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute auth={true}>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/sign_in"
+        element={
+          <ProtectedRoute>
+            <Form isSignInPage={true} />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/sign_up"
+        element={
+          <ProtectedRoute>
+            <Form isSignInPage={false} />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,6 +3,7 @@ const Input = ({
   name = "",
   type = "text",
   className = "",
+  inputClassname = "",
   isRequired = false,
   placeholder = "",
 }) => {
@@ -14,7 +15,7 @@ const Input = ({
       <input
         type={type}
         id={name}
-        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5`}
+        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${inputClassname}`}
         placeholder={placeholder}
         required={isRequired}
       />
